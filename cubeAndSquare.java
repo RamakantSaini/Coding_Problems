83eafc4fbd49ca0c7aacdaa938437e82e13af719
Ramakant Saini <ramakantsaini7565@gmail.com>
2024-12-25 11:17:52 +0530

    cubeAndSquare.java

--- cubeAndSquare.java
+++ cubeAndSquare.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class cubeAndSquare {
     public static void main(String[] args) {
-        int square=0;
-        int cube=0;
+        double square=0;
+        double cube=0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your number : ");
-        int num=sc.nextInt();
-        square=num*num;
-        cube=square*num;
+        double num=sc.nextInt();
+        square=Math.pow(num,2);
+        cube=Math.pow(num,3);
         System.out.println("Square is : "+ square);
         System.out.println("Cube is : "+ cube);
         
